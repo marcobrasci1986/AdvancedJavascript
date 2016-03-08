ca1049ad0d98123f9c01ce11d8f5ebfc518689f1
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-03-08 10:49:46 +0100

    Pristine copy

---
+++ exercises/closure/ex1.js
@@ -0,0 +1,13 @@
+function foo() {
+    var bar = "bar";
+
+    function baz() {
+        console.log(bar);
+    }
+
+    bam(baz);
+}
+function bam(baz) {
+    baz();
+}
+foo();
---
+++ exercises/closure/ex3.js
@@ -0,0 +1,6 @@
+var foo = (function () {
+    var o = {bar: 'bar'};
+    return {obj: o};
+})();
+
+console.log(foo.obj.bar);
